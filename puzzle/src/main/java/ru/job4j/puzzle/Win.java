e927995f1acf4d8796e57fa3747e30c45da4e6b1
SvetlDI <svetlana24.dubrova@gmail.com>
2022-02-10 16:03:34 +0500

    2. Сокобан. Тестовое задание. [#75283 #245814]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,8 +3,8 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean horizontalCheck(int[][] board, int row) {
         boolean rsl = true;
-        for (int cell = 0; cell < board[row].length - 1; cell++) {
-            if (board[row][cell] != board[row][cell + 1]) {
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[row][cell] != 1) {
                 rsl = false;
                 break;
             }
@@ -14,8 +14,8 @@ public class Win {
 
     public static boolean verticalCheck(int[][] board, int column) {
         boolean rsl = true;
-        for (int row = 0; row < board[column].length - 1; row++) {
-            if (board[row][column] != board[row + 1][column]) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 1) {
                 rsl = false;
                 break;
             }
@@ -24,21 +24,11 @@ public class Win {
     }
 
     public static boolean check(int[][] board) {
-        boolean resultHorisontal = true;
-        boolean resultVertical = true;
         for (int i = 0; i < board.length; i++) {
-            if (!horizontalCheck(board, i)) {
-                resultHorisontal = false;
-                break;
+            if (board[i][i] == 1 && (horizontalCheck(board, i) || verticalCheck(board, i))) {
+                return true;
             }
         }
-
-        for (int i = 0; i < board.length; i++) {
-            if (!verticalCheck(board, i)) {
-                resultVertical = false;
-                break;
-            }
-        }
-        return resultHorisontal || resultVertical;
+        return false;
     }
 }
